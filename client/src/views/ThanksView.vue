50a069716c08c5247c9be9560fc56d4d3cc6167d
cup11 <cup11jason@qq.com>
2024-08-24 15:34:01 +0800

    slight fix ThanksView

--- client/src/views/ThanksView.vue
+++ client/src/views/ThanksView.vue
@@ -3,13 +3,13 @@ import router from '@/router/index';
 
 setTimeout(() => {
   router.push('/');
-}, 3000);
+}, 5000);
 </script>
 
 <template>
   <div class="flex flex-col min-h-[70vh] gap-4 py-8 items-center justify-center">
     <div class="text-center text-5xl font-bold">您的报名已提交成功！</div>
-    <img src="/img/thanks-bird.jpg">
-    <div class="text-center text-lg mt-2">3 秒后自动跳回主页查看选课情况</div>
+    <img src="/img/thanks-bird.jpg" width="128">
+    <div class="text-center text-lg mt-2">5 秒后自动跳回主页查看选课情况</div>
   </div>
 </template>
