49d2a121d2554bbc96c1d5085da827bbe55d3ae6
cup11 <cup11jason@qq.com>
2024-09-07 18:12:36 +0800

    todo

---
+++ pb_migrations/1725698902_updated_societies.js
@@ -0,0 +1,33 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("llxyw8vdwy6tiwn")
+
+  // remove
+  collection.schema.removeField("6ehwdeul")
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("llxyw8vdwy6tiwn")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "6ehwdeul",
+    "name": "rejects",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "_pb_users_auth_",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": null,
+      "displayFields": null
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
