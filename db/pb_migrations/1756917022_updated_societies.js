33e2acc208b2f02fe492a0609820c6baa2a7e558
Jason Li <jasonmli@outlook.com>
2025-09-04 00:30:43 +0800

    chore(db): update permission

---
+++ db/pb_migrations/1756917022_updated_societies.js
@@ -0,0 +1,20 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("llxyw8vdwy6tiwn")
+
+  // update collection data
+  unmarshal({
+    "deleteRule": "@request.auth.role = \"teacher\""
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("llxyw8vdwy6tiwn")
+
+  // update collection data
+  unmarshal({
+    "deleteRule": null
+  }, collection)
+
+  return app.save(collection)
+})
